75a015b34d4b941911324fbbb87b1b5b10691374
[Rahul Kumawat] <[rahulkumawat20000@gmail.com]>
2021-12-16 18:07:32 +0530

    JsonApp_Ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,50 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  cats:any;
+  constructor(public http:HttpClient, public router:Router) {
+    this.loadData();
+  }
 
+  ngOnInit() {
+    
+  }
+
+  loadData(){
+    let data:Observable<any>
+    data = this.http.get('assets/backend/db.json');
+    data.subscribe(result => {
+      this.cats = result;
+      console.log(this.cats);
+    },
+    (err) => {
+      alert('failed loading json data');
+   });
+  }
+
+  name:string;
+  origin:string;
+  averagelife:string;
+  breed:string;
+  url:string;
+
+  SendDataFu(){ 
+
+    let postData =  {
+     AltName:this.name,
+     Origin:this.origin,
+     Averagelife:this.averagelife,
+     BreedDesc:this.breed,
+     url:this.url
+ }
+  
+    this.http.post("assets/backend/db.json", postData,{observe: 'response'})
+      .subscribe(data => {
+        console.log(data);
+       
+       }, error => {
+        console.log(error);
+      });
+      
+  }
 }
